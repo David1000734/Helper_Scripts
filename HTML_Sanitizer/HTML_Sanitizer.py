36f2d2c24c58b50bb43db965b9b90a45f73b8f15
David <141961763+David1000734@users.noreply.github.com>
2025-06-25 13:33:40 -0700

    Refactor code and improve file handling
    clean_quotation function added to replace HTML quotations with standard quotes.
    
    Added logic to open and read file contents as well as improving error handling capabilities.
    
    Added exit to menu and improved readability.
    
    Moved HTML_Light.html to test_files directory, deleted an unused file, and added a sample temp.txt for testing.

--- HTML_Sanitizer/HTML_Sanitizer.py
+++ HTML_Sanitizer/HTML_Sanitizer.py
@@ -13,31 +13,55 @@ log = logging.basicConfig(
     level=logging.DEBUG
 )
 
-def clean_file(file: str, remove_styles: bool) -> str:
+def clean_quotation(content: str) -> str:
+    logging.debug(f"\nBefore clean_quotation:\n{content}")
+    return re.sub(r'&[lr].*?;', '"', content)
+
+
+def clean_file(file_name: str, remove_styles: bool) -> str:
     '''
     Function will provide a fully cleaned HTML file.
 
-    :param file: File content
+    :param file_name: File content
     :param remove_styles: Should "styles=..." be removed from <p>, <td> etc.
     '''
 
+    file = open(file_name, "rt")
 
+    content = file.read()
+    content = clean_quotation(content)
 
-    return ""
+    logging.debug(f"\nCleaned Return: {content}\n")
+
+    return content
 
 def specific_file():
-    file_name = input("Please provide the file name: ")
-    logging.debug(f"File provided {file_name}")
+    loop = True
 
-    # Remove the file extention if there is one
-    file_name = os.path.splitext(file_name)[0]
+    while (loop):
+        try:
+            file_name = input("Please provide the file name and extension: ")
+            logging.debug(f"File provided \"{file_name}\"")
 
-    # An exception is thrown if failed to open. file is valid
-    parsed_file = clean_file(file_name, True)
+            if not (file_name.endswith('.html') or file_name.endswith('.txt')):
+                raise ValueError(f"Only .html and .txt files are accepted, file recieved: \"{file_name}\"")
 
-    # Write to file
-    file = open(file_name + ".html", "wt")
-    file.write(parsed_file)
+            # Remove the file extention if there is one
+            # file_name = os.path.splitext(file_name)[0]
+
+            # An exception is thrown if failed to open.
+            parsed_file = clean_file(file_name, True)
+
+            # Write to file
+            file = open(file_name, "wt")
+            file.write(parsed_file)
+
+            loop = False
+        except FileNotFoundError as e:
+            print(f"{e}\n")
+        
+        except ValueError as e:
+            print(f"{e}\n")
 
 
 def files_in_directory():
@@ -48,12 +72,13 @@ def files_in_directory():
 
 
 # User selection
-selection = 0
+selection = -1
 loop = True
 
 print("Hello, welcome to HTML cleaner. Please select an option.")
 while (loop):
     # Menu
+    print("0. Exit")
     print("1. Select a specific file.")
     print("2. Clean all HTML files in directory.")
 
@@ -61,7 +86,7 @@ while (loop):
         # Get user input
         selection = int(input())
 
-        if (not(selection < 3 and selection > 0)):
+        if (not(selection < 3 and selection > -1)):
             print("Number is out of range")
         else:
             loop = False
@@ -71,7 +96,13 @@ while (loop):
         logging.warning(f"Unknown error: {e}")
         exit(1)
 
+# Clear screen. Should work with other OS
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
 match selection:
+    case 0:
+        print("Goodbye!")
+
     # Choose specific file
     case 1:
         specific_file()
@@ -80,6 +111,5 @@ match selection:
     case 2:
         files_in_directory()
 
-    case _:
-        logging.warning("You should not be here...")
-        exit(0)
+    case _ as e:
+        logging.warning(f"You should not be here...\n{e}")
